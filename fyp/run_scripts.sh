3811bc4c1b5143fbcdc806b58a5409baf5d9b068
Emile <emile.matta@gmail.com>
2024-11-10 20:29:16 +0200

    added bash script

---
+++ fyp/run_scripts.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Change directory to the Desktop/fyp folder
+cd ~/Desktop/fyp
+
+# Run the Python scripts in order
+sudo python create_ap.py
+sudo python prob.py
+sudo python aut.py
+sudo python assos.py
